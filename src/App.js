513a7673a67c62dc8f591c0ed5a8c6edd8dc35bc
crazy4862 <1sv21is013@shrideviengineering.org>
2023-11-29 10:49:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
+import LoginForm from "./pages/loginform";
 import Pagenotfound from "./pages/Pagenotfound";
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
       </BrowserRouter>
